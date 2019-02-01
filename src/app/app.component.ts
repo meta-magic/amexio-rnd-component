b4734c1982942fb00a3200842520f1a0d73e67f1
sagarjadhav968 <sagarjdhv968@gmail.com>
2019-02-01 17:58:07 +0530

    demo added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,49 +8,46 @@ import { ControlContainer, NgForm } from '@angular/forms';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  empid = "2";
-  @ViewChild(NgForm) public form: NgForm;
-  //empid:string;
-  empdata : Employee [];
-  empname : string;
-  selectedItems: any [];
-
-  employee: EmployeeModel;
-  constructor(){
-    this.empdata = [{"id":"1","firstName":"Araf","lastName":"Karsh","genderId":"1","age":35,"phone":"0987654321","email":"abcd@abc.com","departmentId":"1","countryId":"356","stateId":"3","countryName":"India","stateName":"Kerala","gender":"Male","deptName":"Admin"},{"id":"2","firstName":"Ketan","lastName":"Gote","genderId":"1","age":35,"phone":"0987654321","email":"abcd@abc.com","departmentId":"2","countryId":"356","stateId":"1","countryName":"India","stateName":"Maharastra","gender":"Male","deptName":"HR"},{"id":"3","firstName":"Akhtar","lastName":"Jadhav","genderId":"1","age":25,"phone":"0987654321","email":"abcd@abc.com","departmentId":"3","countryId":"356","stateId":"6","countryName":"India","stateName":"Rajasthan","gender":"Male","deptName":"Finance"},{"id":"4","firstName":"Dattaram","lastName":"Gawas","genderId":"1","age":25,"phone":"0987654321","email":"abcd@abc.com","departmentId":"1","countryId":"356","stateId":"1","countryName":"India","stateName":"Maharastra","gender":"Male","deptName":"Admin"},{"id":"5","firstName":"Rashmi","lastName":"Thakkar","genderId":"2","age":25,"phone":"0987654321","email":"abcd@abc.com","departmentId":"3","countryId":"356","stateId":"4","countryName":"India","stateName":"Gujrat","gender":"Female","deptName":"Finance"},{"id":"6","firstName":"Deepali","lastName":"Arvind","genderId":"2","age":25,"phone":"0987654321","email":"abcd@abc.com","departmentId":"2","countryId":"356","stateId":"1","countryName":"India","stateName":"Maharastra","gender":"Female","deptName":"HR"},{"id":"7","firstName":"Ashwini","lastName":"Arge","genderId":"2","age":25,"phone":"0987654321","email":"abcd@abc.com","departmentId":"1","countryId":"356","stateId":"1","countryName":"India","stateName":"Maharastra","gender":"Female","deptName":"Admin"},{"id":"8","firstName":"Manisha","lastName":"Boddu","genderId":"2","age":25,"phone":"0987654321","email":"abcd@abc.com","departmentId":"3","countryId":"356","stateId":"5","countryName":"India","stateName":"Tamil Nadu","gender":"Female","deptName":"Finance"},{"id":"9","firstName":"Ankita","lastName":"Jain","genderId":"2","age":25,"phone":"0987654321","email":"abcd@abc.com","departmentId":"3","countryId":"356","stateId":"1","countryName":"India","stateName":"Maharastra","gender":"Female","deptName":"Finance"},{"id":"10","firstName":"Kedar","lastName":"Kokil","genderId":"2","age":25,"phone":"0987654321","email":"abcd@abc.com","departmentId":"2","countryId":"356","stateId":"1","countryName":"India","stateName":"Maharastra","gender":"Female","deptName":"HR"}];
-    this.employee = new EmployeeModel();
-  }
-
-  addNewRecord(event:any){
-    this.empdata.push(new Employee(new Date().getTime()+"","New Record "+new Date().getTime(),"Karsh","1",35,"0987654321","abcd@abc.com","1","356","3","India","Kerala","Male","Admin"));
-  }
-
-  ngAfterViewChecked() {
-  }
+  contactList: any = [];
+  constructor() {
+    this.contactList = [
+      {
+        'name': 'Ketan Gote',
+        'description': 'UI/UX, Microservice, Kubernetes, Docker ',
+        'imagepath': 'https://i.ibb.co/zxvy25m/ketan.jpg',
+        'bgcolor': 'linear-gradient(40deg,#12c2e9, #ec38bc)'
+      },
+      {
+      'name': 'Sagar Jadhav',
+      'description': 'Angular, Java, Microservices',
+      'imagepath': 'https://media.licdn.com/dms/image/C5103AQF3EDkCcxqsnQ/profile-displayphoto-shrink_100_100/0?e=1554336000&v=beta&t=RhxgAziI4yMWqftaeq6WPvMlSKJgWSzZH5c6H5zT6Yc'
+      'bgcolor': 'linear-gradient(40deg,#141E30,#243B55)'
+    },
+    {
+      'name': 'Dattaram Gawas',
+      'description': 'UI / UX Desinger',
+      'imagepath': 'https://i.ibb.co/J7QyC3g/dats.jpg',
+      'bgcolor': 'linear-gradient(40deg,#a8ff78, #78ffd6)'
+    },
+    {
+      'name': 'Rashmi Thakker',
+      'description': 'UI Developer',
+      'imagepath': 'https://i.ibb.co/cJSjBtd/rashmi.jpg',
+      'bgcolor': 'linear-gradient(40deg,#457fca, #5691c8)'
+    },
+    {
+      'name': 'Ashwini Agre',
+      'description': 'UI Developer',
+      'imagepath': 'https://i.ibb.co/th1kSKq/ashwini.jpg',
+      'bgcolor': 'linear-gradient(40deg,#bc4e9c, #f80759)'
+    },
+    {
+      'name': 'Deepali Arvind',
+      'description': 'Security Analyst',
+      'imagepath': 'https://i.ibb.co/3CTVNq8/dipali.jpg',
+      'bgcolor': 'linear-gradient(40deg,#40E0D0, #FF0080)'
+    }
+    ];
+ }
 }
 
-
-class EmployeeModel {
-  firstname: string;
-  lastname: string;
-  home: Address;
-  office: Address;
-
-  constructor(){
-    
-    this.home = new Address();
-    this.office = new Address();
-    this.firstname = "Ketan";
-    this.lastname = "Gote";
-    this.home.addressline = "Addressline Home";
-    this.home.pincode = "123123";
-    this.office.addressline = "Addressline Office";
-    this.office.pincode = "345345";
-
-  }
-}
-
-class Address{
-  addressline : string;
-  pincode : string;
-}
